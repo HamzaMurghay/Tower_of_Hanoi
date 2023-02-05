6e6c03f3575d696b9ca1188b38e03a92bc8c5249
Hamza Murghay <113519711+HamzaMurghay@users.noreply.github.com>
2023-02-05 06:59:29 +0530

    Explanation of pattern to determine minimum no. of moves

--- TOH_optimalmove_calculator.py
+++ TOH_optimalmove_calculator.py
@@ -21,13 +21,20 @@ def toh_movecount(n):
 
 
 
+""" if you want to understand how this function works, comment out the above program and run the program below
+  it will make it easier to understand, although the explanation bugs out from 4 disks onwards you should
+  be able to understand it by the first three disks"""
 
-
-
-
-# if you want to understand how this function works, comment out the above program and run the program below
-# it will make it easier to understand, although the explanation bugs out from 4 disks onwards you should
-# be able to understand it by the first three disks
+"""Essentially, the pattern for no. of disks to no. of moves is as follows:
+   1 disk = 1 move
+   2 disks = 3 moves
+   3 disks = 7 moves
+   4 disks = 15 moves
+   5 disks = 31 moves
+   6 disks = 63 moves
+   
+   The common pattern here is that, for the nth disk you need to add n + the no. of moves it took for all of the previous disks to solve their respectice towers i.e
+   for 4 disks its = 4 + (1 + 3 + 7) and for 5 disks its = 5 + (1 + 3 + 7 + 15)"""
 
 
 # def toh_movecount(n):
